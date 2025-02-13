09f7e1b43f95c8420492d880ebd68dd52f23127a
Lana150 <astra.altair150@gmail.com>
2025-02-13 04:13:51 +0100

    add

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -17,22 +17,3 @@ const AppBar = () => {
 };
 
 export default AppBar;
-
-
-/*import Navigation from "../Navigation/Navigation";
-import AuthNav from "../AuthNav/AuthNav";
-import UserMenu from "../UserMenu/UserMenu";
-import { useSelector } from "react-redux";
-import { selectIsLoggedIn } from "../../redux/auth/selectors";
-
-const AppBar = () => {
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-
-    return (
-    <header>
-      <Navigation />
-      {isLoggedIn ? <UserMenu /> : <AuthNav />}
-    </header>
-     );
-};
- */
