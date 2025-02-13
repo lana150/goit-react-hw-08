e91ed8965b7a3de19653201d3315ed4b26e5d392
Lana150 <astra.altair150@gmail.com>
2025-02-13 04:26:14 +0100

    add

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,7 +2,7 @@ import s from './HomePage.module.css';
 
 const HomePage = () => (
   <div className={s.homePage}>
-    <h1 className={s.title}>Welcome to Phonebook App</h1>
+    <h1 className={s.title}>Welcome to Phonebook App!</h1>
     <p className={s.description}>
       Manage your contacts efficiently and securely.
     </p>
