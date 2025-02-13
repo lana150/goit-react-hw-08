1e7e3861d04f36c45cf602f66cda52be1a53a7e5
Lana150 <astra.altair150@gmail.com>
2025-02-13 17:51:06 +0100

    add

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,4 +1,66 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchContacts } from "../../redux/contacts/operations";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
 import ContactForm from "../../components/ContactForm/ContactForm";
+import ContactList from "../../components/ContactList/ContactList";
+import styles from "./ContactsPage.module.css";
+
+const ContactsPage = () => {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      dispatch(fetchContacts());
+    }
+  }, [dispatch, isLoggedIn]);
+
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Your Contacts</h2>
+      <ContactForm />
+      <ContactList />
+    </div>
+  );
+};
+
+export default ContactsPage;
+
+
+
+
+
+/*import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchContacts } from "../../redux/contacts/operations"; 
+import ContactForm from "../../components/ContactForm/ContactForm";
+import ContactList from "../../components/ContactList/ContactList";
+import styles from "./ContactsPage.module.css";
+
+const ContactsPage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log("Fetching contacts...");
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Your Contacts</h2>
+      <ContactForm />
+      <ContactList />
+    </div>
+  );
+};
+
+export default ContactsPage;*/
+
+
+
+
+/*import ContactForm from "../../components/ContactForm/ContactForm";
 import ContactList from "../../components/ContactList/ContactList";
 import styles from "./ContactsPage.module.css";
 
@@ -12,4 +74,4 @@ const ContactsPage = () => {
   );
 };
 
-export default ContactsPage;
+export default ContactsPage;*/
